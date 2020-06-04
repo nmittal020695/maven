0def05ccc343fa9e3eb61eaaaa6b048d9653b157
nmittal020695 <65939616+nmittal020695@users.noreply.github.com>
2020-06-04 19:44:31 +0530

    new comment

---
+++ maventest/src/main/java/mavenTest/maventest/App.java
@@ -0,0 +1,13 @@
+package mavenTest.maventest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
